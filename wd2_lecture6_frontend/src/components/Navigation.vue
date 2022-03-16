b5437d60e94e8c768aa4fb8355e1708ad651cd43
662781 <662781@student.inholland.nl>
2022-03-16 11:04:22 +0100

    Update Navigation.vue

--- wd2_lecture6_frontend/src/components/Navigation.vue
+++ wd2_lecture6_frontend/src/components/Navigation.vue
@@ -4,8 +4,12 @@
       <ul class="navbar-nav me-auto mb-2 mb-md-0">
         <li class="nav-item">
           <!-- Add a router link to the homepage (don't use the a tag!) -->
-          <router-link v-if="this.$store.getters.isAuthenticated" to="/profile" class="nav-link" active-class="active"
-            >{{this.$store.state.username}}</router-link
+          <router-link
+            v-if="this.$store.getters.isAuthenticated"
+            to="/profile"
+            class="nav-link"
+            active-class="active"
+            >{{ this.$store.state.username }}</router-link
           >
         </li>
         <li class="nav-item">
@@ -13,8 +17,8 @@
           <router-link to="/" class="nav-link" active-class="active"
             >Home</router-link
           >
-        </li>      
-        <li class="nav-item">
+        </li>
+        <li class="nav-item" v-if="this.$store.getters.isAuthenticated">
           <!-- add a router link to the products page (don't use the a tag!) -->
           <router-link to="/products" class="nav-link" active-class="active"
             >Products</router-link
@@ -26,10 +30,16 @@
             >Login</router-link
           >
         </li>
+      </ul>
+      <ul class="navbar-nav ml-auto">
         <li class="nav-item" v-if="this.$store.getters.isAuthenticated">
           <!-- Make and use a logout component! -->
-          <a @click="this.$store.dispatch('logout')" class="nav-link" active-class="active"
-            >Logout</a>
+          <a
+            @click="this.$store.dispatch('logout')"
+            class="nav-link"
+            active-class="active"
+            >Logout</a
+          >
         </li>
       </ul>
     </div>
